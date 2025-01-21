a45192a90a002b6cf1d41d15374defdc6d5514ca
Alexis <alexis.martinho.78@gmail.com>
2025-01-21 13:16:36 +0100

    Input

---
+++ Source/BallPlatformer/Private/BPBall.cpp
@@ -0,0 +1,71 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BallPlatformer/Public/BPBall.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+
+
+// Sets default values
+ABPBall::ABPBall()
+{
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("Sphere Collision"));
+	RootComponent = SphereComponent;
+	SphereComponent->SetSimulatePhysics(true);
+
+	StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	StaticMeshComponent->SetupAttachment(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void ABPBall::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Add Mapping Contexte
+    if (const auto PlayerController = Cast<APlayerController>(Controller))
+    {
+		if (UEnhancedInputLocalPlayerSubsystem* PlayerSubsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+     	{
+     		if (!MappingContext.IsNull())
+     		{
+				PlayerSubsystem->AddMappingContext(MappingContext.LoadSynchronous(), 0);
+     		}
+     	}
+    }
+}
+
+// Called every frame
+void ABPBall::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ABPBall::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	
+	UEnhancedInputComponent* EnhancedInput = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+	EnhancedInput->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ABPBall::MoveFunc);
+	EnhancedInput->BindAction(JumpAction, ETriggerEvent::Triggered, this, &ABPBall::JumpFunc);
+}
+
+void ABPBall::MoveFunc(const FInputActionValue& Value)
+{
+	const FVector MovementVector = Value.Get<FVector>();
+	GEngine->AddOnScreenDebugMessage(-1, 0.1f, FColor::Cyan, TEXT("Move Action"));
+	SphereComponent->AddForce(MoveForce * MovementVector);
+}
+
+void ABPBall::JumpFunc(const FInputActionValue& Value)
+{
+	const bool JumpValue = Value.Get<bool>();
+	GEngine->AddOnScreenDebugMessage(-1, 0.1f, FColor::Cyan, TEXT("Jump Action"));
+	SphereComponent->AddImpulse(JumpImpulse * GetActorUpVector());
+}
+
---
+++ Source/BallPlatformer/Public/BPBall.h
@@ -0,0 +1,58 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InputMappingContext.h"
+#include "Components/SphereComponent.h"
+#include "GameFramework/Pawn.h"
+#include "BPBall.generated.h"
+
+UCLASS()
+class BALLPLATFORMER_API ABPBall : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ABPBall();
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* StaticMeshComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	USphereComponent* SphereComponent;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(EditDefaultsOnly, Category="Input")
+	TSoftObjectPtr<UInputMappingContext> MappingContext;
+
+	UPROPERTY(EditDefaultsOnly, Category="Input")
+	UInputAction* MoveAction;
+
+	UPROPERTY(EditDefaultsOnly, Category="Input")
+	UInputAction* JumpAction;
+
+	UPROPERTY(EditAnywhere, Category="Movement")
+	float MoveForce = 400.f;
+
+	UPROPERTY(EditAnywhere, Category="Movement")
+	float JumpImpulse = 60.f;
+
+	UFUNCTION()
+	void MoveFunc(const FInputActionValue& Value);
+
+	UFUNCTION()
+	void JumpFunc(const FInputActionValue& Value);
+};
